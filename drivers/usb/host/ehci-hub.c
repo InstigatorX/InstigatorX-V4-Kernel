aca4ff7cd45f82e74df1f7abff706faa83348189
Boris Todorov <boris.st.todorov@gmail.com>
2011-07-11 12:03:33 +0300

    USB: EHCI: Fix test mode sequence
    commit 77636c86a600b83de01719efad83567e46d7e8ce upstream.
    
    The sequence to put port in test mode is not complete.
    According EHCI specification all enabled ports must be
    put in suspend.
    
    Signed-off-by: Boris Todorov <boris.st.todorov@gmail.com>
    Acked-by: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>
    
    Conflicts:
    
    	drivers/usb/host/ehci-hub.c
    
    Signed-off-by: InstigatorX <sloebric@gmail.com>

--- drivers/usb/host/ehci-hub.c
+++ drivers/usb/host/ehci-hub.c
@@ -1289,6 +1289,25 @@ static int ehci_hub_control (
 #endif
 			else
 				goto error;
+<<<<<<< HEAD
+=======
+			ehci_quiesce(ehci);
+
+			/* Put all enabled ports into suspend */
+			while (ports--) {
+				u32 __iomem *sreg =
+						&ehci->regs->port_status[ports];
+
+				temp = ehci_readl(ehci, sreg) & ~PORT_RWC_BITS;
+				if (temp & PORT_PE)
+					ehci_writel(ehci, temp | PORT_SUSPEND,
+							sreg);
+			}
+			ehci_halt(ehci);
+			temp = ehci_readl(ehci, status_reg);
+			temp |= selector << 16;
+			ehci_writel(ehci, temp, status_reg);
+>>>>>>> 00c37d5... USB: EHCI: Fix test mode sequence
 			break;
 
 		default:
