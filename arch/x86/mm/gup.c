26f4fce156ddaf5353b1383a1e1d302d6cfab607
Andrea Arcangeli <aarcange@redhat.com>
2011-11-02 13:37:36 -0700

    thp: share get_huge_page_tail()
    commit b35a35b556f5e6b7993ad0baf20173e75c09ce8c upstream.
    
    This avoids duplicating the function in every arch gup_fast.
    
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Johannes Weiner <jweiner@redhat.com>
    Cc: Rik van Riel <riel@redhat.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: KOSAKI Motohiro <kosaki.motohiro@jp.fujitsu.com>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: David Gibson <david@gibson.dropbear.id.au>
    Cc: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Cc: Heiko Carstens <heiko.carstens@de.ibm.com>
    Cc: David Miller <davem@davemloft.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>
    
    Conflicts:
    
    	arch/x86/mm/gup.c
    	include/linux/mm.h
    
    Signed-off-by: InstigatorX <sloebric@gmail.com>

--- arch/x86/mm/gup.c
+++ arch/x86/mm/gup.c
@@ -108,6 +108,7 @@ static inline void get_head_page_multiple(struct page *page, int nr)
 	SetPageReferenced(page);
 }
 
+<<<<<<< HEAD
 static inline void get_huge_page_tail(struct page *page)
 {
 	/*
@@ -118,6 +119,8 @@ static inline void get_huge_page_tail(struct page *page)
 	atomic_inc(&page->_count);
 }
 
+=======
+>>>>>>> 621112e... thp: share get_huge_page_tail()
 static noinline int gup_huge_pmd(pmd_t pmd, unsigned long addr,
 		unsigned long end, int write, struct page **pages, int *nr)
 {
