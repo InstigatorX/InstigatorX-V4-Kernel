28fef57445afc9367317e73fd4fe3c33ce1e2d82
Dima Zavin <dima@android.com>
2012-04-30 10:26:14 +0100

    ARM: 7409/1: Do not call flush_cache_user_range with mmap_sem held
    commit 435a7ef52db7d86e67a009b36cac1457f8972391 upstream.
    
    We can't be holding the mmap_sem while calling flush_cache_user_range
    because the flush can fault. If we fault on a user address, the
    page fault handler will try to take mmap_sem again. Since both places
    acquire the read lock, most of the time it succeeds. However, if another
    thread tries to acquire the write lock on the mmap_sem (e.g. mmap) in
    between the call to flush_cache_user_range and the fault, the down_read
    in do_page_fault will deadlock.
    
    [will: removed drop of vma parameter as already queued by rmk (7365/1)]
    
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Dima Zavin <dima@android.com>
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Conflicts:
    
    	arch/arm/kernel/traps.c

--- arch/arm/kernel/traps.c
+++ arch/arm/kernel/traps.c
@@ -477,6 +477,7 @@ do_cache_op(unsigned long start, unsigned long end, int flags)
 			end = vma->vm_end;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 		up_read(&mm->mmap_sem);
 		flush_cache_user_range(start, end);
 
@@ -487,6 +488,11 @@ do_cache_op(unsigned long start, unsigned long end, int flags)
 =======
 		flush_cache_user_range(start, end);
 >>>>>>> 866cd76... ARM: 7365/1: drop unused parameter from flush_cache_user_range
+=======
+		up_read(&mm->mmap_sem);
+		flush_cache_user_range(start, end);
+		return;
+>>>>>>> ab55458... ARM: 7409/1: Do not call flush_cache_user_range with mmap_sem held
 	}
 	up_read(&mm->mmap_sem);
 }
