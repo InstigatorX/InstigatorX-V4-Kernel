d9b17b8b81e665c33c438bd4a9865907816f7b4a
InstigatorX <sloebric@gmail.com>
2012-07-17 23:07:53 -0700

    OC: GPU OC
    Updated GPU OC
    GPU is stock clocked at 3D@266 & 2D@200.
    
    To increase maximum GPU frequency in 3D mode to 320 MHz:
    echo 320000000 >
    /sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/max_gpuclk
    
    To increase maximum GPU frequency in 2D mode to 266 MHz:
    echo 266667000 >
    /sys/devices/platform/kgsl-2d0.0/kgsl/kgsl-2d0/max_gpuclk
    echo 266667000 >
    /sys/devices/platform/kgsl-2d1.1/kgsl/kgsl-2d1/max_gpuclk
    
    Available frequencies:
    3D: 320000000, 300000000, 266667000, 228571000, 200000000, 177778000,
    27000000
    2D: 266667000, 228571000, 200000000, 160000000, 96000000, 27000000

--- include/linux/msm_kgsl.h
+++ include/linux/msm_kgsl.h
@@ -34,6 +34,8 @@
 #define KGSL_CLK_MEM_IFACE 0x00000010
 #define KGSL_CLK_AXI	0x00000020
 
+#define KGSL_MAX_PWRLEVELS 7
+
 /*
  * Reset status values for context
  */
@@ -154,6 +156,7 @@ struct kgsl_version {
 struct kgsl_device_platform_data {
 	struct kgsl_pwrlevel pwrlevel[KGSL_MAX_PWRLEVELS];
 	int init_level;
+	int max_level;
 	int num_levels;
 	int (*set_grp_async)(void);
 	unsigned int idle_timeout;
