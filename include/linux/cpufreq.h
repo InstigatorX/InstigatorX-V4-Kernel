de2811263ccd1014cc9dcdc286945a2004c52111
InstigatorX <sloebric@gmail.com>
2012-08-20 16:30:06 -0700

    GOV: Added Abyssplug

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -487,6 +487,9 @@ extern struct cpufreq_governor cpufreq_gov_lagfree;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
 extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_pegasusq)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ABYSSPLUG)
+extern struct cpufreq_governor cpufreq_gov_abyssplug;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_abyssplug)
 #endif
 
 
